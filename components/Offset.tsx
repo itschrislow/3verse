1c57861e670af53b7d75ffe191640d6342720e0d
chris0610 <chrislow360@gmail.com>
2022-05-13 21:27:29 +0800

    create offset component

---
+++ components/Offset.tsx
@@ -0,0 +1,10 @@
+const Offset = ({}) => (
+  <div className="h-full">
+    <p className="mb-4 text-sm">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pulvinar
+      dui ipsum, sed fermentum augue iaculis et.
+    </p>
+  </div>
+);
+
+export default Offset;
