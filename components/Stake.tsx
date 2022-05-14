81c8e392f444ae00925af3909a2e04ea828a0631
chris0610 <chrislow360@gmail.com>
2022-05-14 11:29:55 +0800

    replace placeholder text for stake component

--- components/Stake.tsx
+++ components/Stake.tsx
@@ -14,10 +14,7 @@ const Stake = ({ isWalletConnected, handleConnectWallet }: StakeProps) => {
 
   return (
     <div className="flex h-full flex-col gap-4">
-      <p className="text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pulvinar
-        dui ipsum, sed fermentum augue iaculis et.
-      </p>
+      <p className="text-sm">Stake your 3verse to earn O² tokens</p>
       <div className="grow">
         {/* TAB */}
         <div className="w-full">
@@ -49,23 +46,54 @@ const Stake = ({ isWalletConnected, handleConnectWallet }: StakeProps) => {
               type="number"
               name="amount"
               id="amount"
-              className="focus:border-primary focus:ring-primary block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm"
               placeholder={`Amount to ${isStake === 0 ? "stake" : "unstake"}`}
             />
           </div>
         </div>
-        {/* STAKING INFO */}
-        <div className="pt-4">
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            pulvinar dui ipsum, sed fermentum augue iaculis et.
+        <div className="mt-4 divide-y divide-white/20 border-y border-white/20">
+          {/* STAKING INFO */}
+          <p className="py-2 text-center text-xl font-semibold">Rewards</p>
+          <div className="grid w-full grid-cols-3 gap-4 py-4 text-center">
+            <div>
+              <p className="font-semibold">Annual Yield</p>
+              <p>105 O² Tokens</p>
+            </div>
+            <div>
+              <p className="font-semibold">Monthly Yield</p>
+              <p>8.75 O² Tokens</p>
+            </div>
+            <div>
+              <p className="font-semibold">Daily Yield</p>
+              <p>0.29 O² Tokens</p>
+            </div>
+          </div>
+          {/* BREAKDOWN */}
+          <p className="py-2 text-center text-xl font-semibold">Breakdown</p>
+          <div className="grid w-full grid-cols-3 gap-4 py-4 text-center">
+            <div>
+              <p className="font-semibold">1 tree takes up</p>
+              <p>5m²</p>
+            </div>
+            <div>
+              <p className="font-semibold">1 plot is</p>
+              <p>25m²</p>
+            </div>
+            <div>
+              <p className="font-semibold">CO² Offset per tree is</p>
+              <p>21kg</p>
+            </div>
+          </div>
+          <p className="py-2 text-center text-xl">
+            Annual CO² offset per plot is{" "}
+            <span className="font-semibold">102kg</span>
           </p>
         </div>
       </div>
       {/* BUTTON */}
       <button
         onClick={() => (isWalletConnected ? null : handleConnectWallet())}
-        className="bg-primary mt-2 w-full self-end rounded-lg px-4 py-2.5 text-sm font-medium leading-5 text-white"
+        className="mt-2 w-full self-end rounded-lg bg-primary px-4 py-2.5 text-sm font-medium leading-5 text-white"
       >
         {!isWalletConnected
           ? "Connect Wallet"
