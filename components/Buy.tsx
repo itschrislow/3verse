18ee6a6d90fe28213533569b539b981dd4eaf7a5
chris0610 <chrislow360@gmail.com>
2022-05-15 09:56:26 +0800

    update copywriting and image for buy component

--- components/Buy.tsx
+++ components/Buy.tsx
@@ -10,48 +10,43 @@ const Buy = ({ handleTabChange }: BuyProps) => {
   const [landId, setLandId] = useState<number>();
 
   return (
-    <div>
-      <p className="mb-4 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pulvinar
-        dui ipsum, sed fermentum augue iaculis et.
-      </p>
-      <div className="flex gap-6">
-        <div className="min-h-full w-1/2">
-          <div className="grid-rows-30 grid w-full grid-cols-20 divide-x divide-y divide-white/30 border-r border-b border-white/30  bg-forest-map bg-contain bg-left bg-no-repeat">
-            {Array(600)
-              .fill(0)
-              .map((_, index) => (
-                <div
-                  onClick={() => setLandId(index)}
-                  className={`
-                  aspect-square cursor-pointer hover:bg-white/50
-                  ${landId === index ? "bg-white/70" : ""}
-                  ${index === 0 ? "border-t border-l border-white/30" : ""}
+    <div className="flex h-full flex-col">
+      <p className="mb-4 text-sm">Buy TREE tokens directly on our platform.</p>
+      <div className="flex h-full items-start justify-center gap-6">
+        <div className="grid-rows-20 grid w-full grid-cols-20 divide-x divide-y divide-white/20 border-r border-b border-white/30  bg-island bg-contain bg-left bg-no-repeat">
+          {Array(400)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                onClick={() => setLandId(index)}
+                className={`
+                  aspect-square cursor-pointer hover:bg-white/30
+                  ${landId === index ? "bg-white/50" : ""}
+                  ${index === 0 ? "border-t border-l border-white/20" : ""}
                 `}
-                  key={index}
-                />
-              ))}
-          </div>
+                key={index}
+              />
+            ))}
         </div>
         <div className="w-1/2">
-          {/* STEP 1 */}
           <div className="flex flex-col gap-4">
+            {/* STEP 1 */}
             <div className="flex items-center gap-4">
               <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-white">
                 <span className="text-white">1</span>
               </span>
-              <p>Select GreenLand</p>
+              <p>Select TREE token</p>
             </div>
             {/* STEP 2 */}
             <div className="flex items-center gap-4">
               <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-white">
                 <span className="text-white">2</span>
               </span>
-              <p>Mint GreenLand {landId && "#" + landId}</p>
+              <p>Buy TREE token {landId && "#" + landId}</p>
             </div>
             <div className="pl-12">
-              <button className="bg-primary w-full self-end rounded-lg px-4 py-2 text-sm font-medium text-white">
-                Mint
+              <button className="w-full self-end rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white">
+                Buy
               </button>
             </div>
             {/* STEP 3 */}
@@ -59,12 +54,12 @@ const Buy = ({ handleTabChange }: BuyProps) => {
               <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-white">
                 <span className="text-white">3</span>
               </span>
-              <p>Stake GreenLand {landId && "#" + landId}</p>
+              <p>Stake TREE token {landId && "#" + landId}</p>
             </div>
             <div className="pl-12">
               <button
                 onClick={() => handleTabChange(TABS.Stake)}
-                className="bg-primary w-full self-end rounded-lg px-4 py-2 text-sm font-medium text-white"
+                className="w-full self-end rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white"
               >
                 Stake
               </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,11 @@ module.exports = {
         primary: "#77bb3f",
       },
       backgroundImage: {
-        "forest-map": "url('/static/map.png')",
+        island: "url('/static/island.png')",
         article: "url('/static/article.jpg')",
       },
       gridTemplateColumns: {
-        16: "repeat(16, minmax(0, 1fr))",
         20: "repeat(20, minmax(0, 1fr))",
-        25: "repeat(25, minmax(0, 1fr))",
-        28: "repeat(28, minmax(0, 1fr))",
-        30: "repeat(30, minmax(0, 1fr))",
       },
     },
   },
