f90e05031bb1af975dad3ced33ca57941f27a00e
chris0610 <chrislow360@gmail.com>
2022-05-21 14:25:45 +0800

    display connect wallet modal for relevant buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,52 +7,72 @@ import Offset from "../components/Offset";
 import Buy from "../components/Buy";
 import Stake from "../components/Stake";
 import Exchange from "../components/Exchange";
+import Modal from "../components/Modal";
+import ConnectWalletModal from "../components/Modal/ConnectWalletModal";
 
 const Home: NextPage = () => {
   const [tab, setTab] = useState(TABS.Buy);
   const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [showConnectWalletModal, setShowConnectWalletModal] = useState(false);
 
   const [plotBalance, setPlotBalance] = useState(2);
   const [tokenBalance, setTokenBalance] = useState(18.17);
 
-  const handleConnectWallet = () => setIsWalletConnected(true);
+  const handleOpenConnectWalletModal = () => setShowConnectWalletModal(true);
+  const handleCloseConnectWalletModal = () => setShowConnectWalletModal(false);
+
+  const handleConnectWallet = () => {
+    setIsWalletConnected(true);
+    handleOpenConnectWalletModal();
+  };
+
   const handleDisconnectWallet = () => setIsWalletConnected(false);
 
   const handleTabChange = (tab: TABS) => setTab(tab);
   const addPlotBalance = () => setPlotBalance(plotBalance + 1);
 
   return (
-    <Layout
-      currTab={tab}
-      handleTabChange={handleTabChange}
-      isWalletConnected={isWalletConnected}
-      handleConnectWallet={handleConnectWallet}
-      handleDisconnectWallet={handleDisconnectWallet}
-      plotBalance={plotBalance}
-      tokenBalance={tokenBalance}
-    >
-      {tab === TABS.Buy && (
-        <Buy
-          handleTabChange={handleTabChange}
-          isWalletConnected={isWalletConnected}
-          handleConnectWallet={handleConnectWallet}
-          addPlotBalance={addPlotBalance}
+    <>
+      <Layout
+        currTab={tab}
+        handleTabChange={handleTabChange}
+        isWalletConnected={isWalletConnected}
+        handleConnectWallet={handleConnectWallet}
+        handleDisconnectWallet={handleDisconnectWallet}
+        plotBalance={plotBalance}
+        tokenBalance={tokenBalance}
+      >
+        {tab === TABS.Buy && (
+          <Buy
+            handleTabChange={handleTabChange}
+            isWalletConnected={isWalletConnected}
+            handleConnectWallet={handleConnectWallet}
+            addPlotBalance={addPlotBalance}
+          />
+        )}
+        {tab === TABS.Stake && (
+          <Stake
+            isWalletConnected={isWalletConnected}
+            handleConnectWallet={handleConnectWallet}
+          />
+        )}
+        {tab === TABS.Offset && (
+          <Offset
+            isWalletConnected={isWalletConnected}
+            handleConnectWallet={handleConnectWallet}
+          />
+        )}
+        {tab === TABS.Exchange && <Exchange />}
+      </Layout>
+      <Modal
+        isOpen={showConnectWalletModal}
+        closeModal={handleCloseConnectWalletModal}
+      >
+        <ConnectWalletModal
+          handleCloseConnectWalletModal={handleCloseConnectWalletModal}
         />
-      )}
-      {tab === TABS.Stake && (
-        <Stake
-          isWalletConnected={isWalletConnected}
-          handleConnectWallet={handleConnectWallet}
-        />
-      )}
-      {tab === TABS.Offset && (
-        <Offset
-          isWalletConnected={isWalletConnected}
-          handleConnectWallet={handleConnectWallet}
-        />
-      )}
-      {tab === TABS.Exchange && <Exchange />}
-    </Layout>
+      </Modal>
+    </>
   );
 };
 
